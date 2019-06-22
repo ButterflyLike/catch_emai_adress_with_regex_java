c27df0c599985e3a0455324f2ad0623669898ab8
familyhappy <toyinling@163.com>
2019-06-22 12:08:45 +0800

    the project dirextionary

---
+++ CatchEmailAddress/src/emailAdress/regex/CatchAddress.java
@@ -0,0 +1,33 @@
+package emailAdress.regex;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CatchAddress {
+	public static void main(String[] args) {
+		try {
+			BufferedReader reader =new  BufferedReader(new FileReader("F:\\test\\email.html"));
+			String line = "";
+			while((line=reader.readLine())!= null) {
+				parese(line);
+			}
+			reader.close();
+			
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	private static void parese(String line) {
+		Pattern pp = Pattern.compile("[\\w[.-]]+@[\\w[.-]]+\\.[\\w]+");
+		Matcher mm = pp.matcher(line);
+		while(mm.find()) {
+			System.out.println(mm.group());
+		}	
+	}
+}
+
